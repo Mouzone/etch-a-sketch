2413ecc7f793ac6859d70450f25848cb9a7ca648
Mouzone <sunnyliu010@gmail.com>
2024-05-23 10:46:03 -0400

    Change flex properties of cells and rows with no min height or width to expand or shrink accoridng to dimensions. Rewrite prompt to exclude values below 1

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 function startUp(){
     const board = document.querySelector(".board")
-
     let length = parseInt(prompt("Enter length (in pixels)"))
-    while ((isNaN(length) || length > 100)) {
+    while ((isNaN(length) || length > 100 || length < 1)) {
         length = parseInt(prompt("Enter valid length"))
     }
 
@@ -14,12 +13,17 @@ function startUp(){
         for (let j = 0; j < length; j++) {
             let cell = document.createElement("div")
             cell.classList.add("cell")
+
             cell.addEventListener("mouseover", event => {
                 event.currentTarget.style.backgroundColor = "black"
             })
             row.appendChild(cell)
         }
     }
+    const rgb_button = document.querySelector("button.rgb")
+    rgb_button.addEventListener("click", event => {
+
+    })
 
     const reset_button = document.querySelector("button.reset")
     reset_button.addEventListener("click", event => {
@@ -30,4 +34,13 @@ function startUp(){
     })
 }
 
+function getRandomRGBColor() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+// set value for color changing button
+// if button is click set
 startUp()
