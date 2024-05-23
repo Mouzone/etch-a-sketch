adf70f22209c938f0269b989af6a2e7d24ba53b5
Mouzone <sunnyliu010@gmail.com>
2024-05-23 11:07:43 -0400

    Refactor code to use functions to generate board and add ink

--- script.js
+++ script.js
@@ -1,29 +1,40 @@
-function startUp(){
-    const board = document.querySelector(".board")
-    let length = parseInt(prompt("Enter length (in pixels)"))
-    while ((isNaN(length) || length > 100 || length < 1)) {
-        length = parseInt(prompt("Enter valid length"))
-    }
-
-    for (let i = 0; i < length; i++){
+function generateBoard(dimension){
+    for (let i = 0; i < dimension; i++){
         let row = document.createElement("div")
         row.classList.add("row")
         board.appendChild(row)
 
-        for (let j = 0; j < length; j++) {
+        for (let j = 0; j < dimension; j++) {
             let cell = document.createElement("div")
             cell.classList.add("cell")
-
-            cell.addEventListener("mouseover", event => {
-                event.currentTarget.style.backgroundColor = "black"
-            })
             row.appendChild(cell)
         }
     }
-    const rgb_button = document.querySelector("button.rgb")
-    rgb_button.addEventListener("click", event => {
+}
 
-    })
+function addInk(rgb){
+    const cells = document.querySelectorAll(".cell")
+    if (!rgb){
+        cells.forEach(cell => {
+            cell.addEventListener("mouseover", event => {
+                cell.style.backgroundColor = "black"
+            })
+        })
+    }
+}
+function startUp(){
+    let dimension = parseInt(prompt("Enter length (in pixels)"))
+    while (isNaN(dimension) || dimension > 100 || dimension < 1) {
+        dimension = parseInt(prompt("Enter valid length"))
+    }
+    generateBoard(dimension)
+    addInk(rgb)
+
+    // const rgb_button = document.querySelector("button.rgb")
+    // rgb_button.addEventListener("click", event => {
+    //     rgb = true
+    //     //reset addInk and the board
+    // })
 
     const reset_button = document.querySelector("button.reset")
     reset_button.addEventListener("click", event => {
@@ -34,13 +45,13 @@ function startUp(){
     })
 }
 
-function getRandomRGBColor() {
-    const r = Math.floor(Math.random() * 256);
-    const g = Math.floor(Math.random() * 256);
-    const b = Math.floor(Math.random() * 256);
-    return `rgb(${r}, ${g}, ${b})`;
-}
+// function getRandomRGBColor() {
+//     const r = Math.floor(Math.random() * 256);
+//     const g = Math.floor(Math.random() * 256);
+//     const b = Math.floor(Math.random() * 256);
+//     return `rgb(${r}, ${g}, ${b})`;
+// }
 
-// set value for color changing button
-// if button is click set
+let rgb = false
+const board = document.querySelector(".board")
 startUp()
