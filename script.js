c6c4c502faf38f0ff452b8a56c9789852a56c1b5
Mouzone <sunnyliu010@gmail.com>
2024-05-23 12:36:35 -0400

    Gradient funciton now works with default icnrement of .1 such that all background colors are rgba(0,0,0,0) and increment by .1 to reach full black

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ function generateBoard(dimension){
         for (let j = 0; j < dimension; j++) {
             let cell = document.createElement("div")
             cell.classList.add("cell")
+            cell.style.backgroundColor = "rgba(0, 0, 0, 0)"
             row.appendChild(cell)
         }
     }
@@ -18,16 +19,20 @@ function getRandomRGBColor() {
     const r = Math.floor(Math.random() * 256);
     const g = Math.floor(Math.random() * 256);
     const b = Math.floor(Math.random() * 256);
-    return `rgb(${r}, ${g}, ${b})`;
+    return `rgba(${r}, ${g}, ${b}, 1)`;
 }
 
-function addInk(rgb){
+function addInk(rgb, gradient){
     const cells = document.querySelectorAll(".cell")
 
     cells.forEach(cell => {
         cell.addEventListener("mouseover", event => {
-            if (!rgb) {
-                cell.style.backgroundColor = "black"
+            if (!rgb && gradient) {
+                let alpha_value = parseFloat(cell.style.backgroundColor.split(',')[3].replace(')', ''))
+                cell.style.backgroundColor =
+                    `rgba(0, 0, 0, ${alpha_value + .1})`
+            } else if (!rgb) {
+                cell.style.backgroundColor = "rgba(0, 0, 0, 1)"
             } else {
                 cell.style.backgroundColor = getRandomRGBColor()
             }
@@ -40,37 +45,41 @@ function startUp(){
     while (isNaN(dimension) || dimension > 100 || dimension < 1) {
         dimension = parseInt(prompt("Enter valid length"))
     }
+
     generateBoard(dimension)
-    addInk(rgb)
+    addInk(rgb, gradient)
 
     const reset_button = document.querySelector("button.reset")
     reset_button.addEventListener("click", event => {
-        const cells = document.querySelectorAll(".cell")
-        cells.forEach(cell=> {
-            cell.style.backgroundColor = "white"
-        })
+        generateBoard(dimension)
+        addInk(rgb, gradient)
     })
 
     const rgb_button = document.querySelector("button.rgb")
     rgb_button.addEventListener("click", event => {
         rgb_button.classList.toggle("active")
+        gradient_button.classList.remove("active")
         rgb = !rgb
         generateBoard(dimension)
-        addInk(rgb)
+        addInk(rgb, gradient)
     })
 
     // do default 10% for now, add slider to select percentage later
+    // make it so gradient will take a value between 0 and 1
+    // when oyu put it in the function even if no gradient add it then floor it to 1 thus opacity will always work
     // only do this for black ink
     const gradient_button = document.querySelector("button.gradient")
     gradient_button.addEventListener("click", event => {
         rgb_button.classList.remove("active")
+        gradient_button.classList.toggle("active")
         rgb = false
-        // opacity = .10
-        // generateBoard(dimension)
-        // addInk(rgb, opacity)
+        gradient = !gradient
+        generateBoard(dimension)
+        addInk(rgb, gradient)
     })
 }
 
 let rgb = false
+let gradient = false
 const board = document.querySelector(".board")
 startUp()
